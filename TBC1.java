40259d23df9a0aa751254f41fc752544e23836da
kurtsikashvilitamar-art <kurtsikashvilitamar@gmail.com>
2026-03-11 23:01:13 +0400

    Add files via upload

---
+++ TBC1.java
@@ -0,0 +1,51 @@
+package test2;
+
+/*
+// davaleba A1
+public class TBC1 {
+    public static void main (String[] args) {
+        int x= 10;
+        int y= 5;
+        int result = 0;
+         if (x>y){
+             result = x*y;
+         }
+         else {
+             result = x+y;}
+        System.out.println(result);
+         }
+    }
+    // 1. შედეგი არის 50. რადგან 10 მეტია 5ზე იმოქმედა if პირობამ და 10 გამრავლდა 5-ზე
+//თუ x-ის მნიშვნელობა ნაკლები იქნებოდა y-ის მნიშვნელობაზე მაშინ შესრულდებოდა else პირობა და შესრულდებოდა მიმატება
+
+// davaleba A2
+public class TBC1 {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 4) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
+// ციკლი გამეორდება მანამდე სანამ პორობა არის ჭეშმარიტი ანუ
+// რადგან count-ის მნიშნელობა 1-ია დაიწყება 1 დან და გაგრძელდება მანამ სანამ 4- ის ტოლი გახდება, ციკლი დატრიალდება 4ჯერ
+// დაიბეჭდება: 1 2 3 4
+
+
+ *///davaleba A3
+public class TBC1 {
+    public static void main(String[] args) {
+        String word = "Java";
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+
+        }
+    }
+}
+//ციკლი იტრიალებს მანამ ამოწურავს "java"-ს ასოების რაოდენოას
+// i=0 რაც ნიშნავს რომ დაიწყოს პირველი ასოდან j დაიბეჭდება: J a v a
+// ციკლი დატრიალდება 4- ჯერ
+
+
+
