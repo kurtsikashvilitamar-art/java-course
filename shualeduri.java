a13a169ab1246201b3af710a8f135df340858c00
kurtsikashvilitamar-art <kurtsikashvilitamar@gmail.com>
2026-03-11 21:17:01 +0400

    Add files via upload

---
+++ shualeduri.java
@@ -0,0 +1,44 @@
+package test2;
+ /*დავალება 41
+public class shualeduri {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello word");
+        }
+    }
+}
+
+
+
+// დავალება 42
+public class shualeduri {
+    public static void shedareba (int x, int y) {
+        System.out.println(x==y);
+
+    }
+        public static void main(String[] args) {
+        shedareba (5, 3);
+        shedareba (2,2);
+      shedareba(14,41);
+       }
+}
+*/
+//დავალება 43
+public class shualeduri {
+    public static void main(String[] args) {
+        String[] cityArray = {"New york", "Sun Francisco", "Dallas", "Los Angeles", "Seattle"};
+        for (int i = 0; i < cityArray.length; i++) {
+            System.out.println(cityArray [i]);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
